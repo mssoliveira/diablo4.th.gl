341df60af2e78e53160aace893c6034cd4136733
lmachens <leon@machens.koeln>
2023-06-12 14:51:24 +0200

    Fix transparent map tiles on desktop

--- app/components/(map)/tiles.tsx
+++ app/components/(map)/tiles.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { WINDOWS } from "@/app/(overwolf)/lib/config";
+import { useCurrentWindow } from "@/app/(overwolf)/lib/windows";
 import { useSettingsStore } from "@/app/lib/storage";
 import { LatLngBoundsExpression } from "leaflet";
 import { useEffect } from "react";
@@ -14,11 +16,13 @@ export const MAX_NATIVE_ZOOM = 6;
 export const TILE_SIZE = 512;
 
 export default function Tiles() {
+  const currentWindow = useCurrentWindow();
   const map = useMap();
   const settingsStore = useSettingsStore();
+  const isOverlay = currentWindow?.name === WINDOWS.OVERLAY;
 
   useEffect(() => {
-    if (settingsStore.overlayMode && settingsStore.overlayTransparentMode) {
+    if (isOverlay && settingsStore.overlayTransparentMode) {
       return;
     }
     const canvasLayer = createCanvasLayer("/map_tiles/{z}/{y}/{x}.webp", {
@@ -35,7 +39,7 @@ export default function Tiles() {
     return () => {
       canvasLayer.remove();
     };
-  }, [settingsStore.overlayTransparentMode]);
+  }, [isOverlay, settingsStore.overlayTransparentMode]);
 
   return <></>;
 }
