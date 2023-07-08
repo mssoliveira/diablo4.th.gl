357fe555e91fdbb32e9f6722d5e49362896aca10
lmachens <leon@machens.koeln>
2023-07-08 16:46:30 +0200

    Fix 2nd screen opacity

--- app/(overwolf)/components/app-container.tsx
+++ app/(overwolf)/components/app-container.tsx
@@ -7,7 +7,9 @@ export default function AppContainer({ children }: { children: ReactNode }) {
   const isOverlay = useGameInfoStore((state) => state.isOverlay);
 
   useEffect(() => {
-    setInputPassThrough(settingsStore.lockedWindow);
+    if (isOverlay) {
+      setInputPassThrough(settingsStore.lockedWindow);
+    }
   }, [settingsStore.lockedWindow]);
 
   return (
@@ -15,10 +17,14 @@ export default function AppContainer({ children }: { children: ReactNode }) {
       className={`h-screen flex flex-col text-white app antialiased select-none overflow-hidden ${
         isOverlay ? (settingsStore.lockedWindow ? "locked" : "") : "bg-black"
       }`}
-      style={{
-        willChange: "opacity",
-        opacity: settingsStore.windowOpacity.toFixed(2),
-      }}
+      style={
+        isOverlay
+          ? {
+              willChange: "opacity",
+              opacity: settingsStore.windowOpacity.toFixed(2),
+            }
+          : {}
+      }
     >
       {children}
     </div>
