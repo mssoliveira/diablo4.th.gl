e785da5f108ee90f72bc5b7906e1241e26cec9bc
lmachens <leon@machens.koeln>
2023-07-28 07:58:31 +0200

    Add name to exported route

--- app/components/routes.tsx
+++ app/components/routes.tsx
@@ -400,76 +400,81 @@ export default function Routes() {
         {routes.routes.length === 0 && (
           <div className="p-2">No routes created</div>
         )}
-        {routes.routes.map((route) => (
-          <article key={route.id} className={`p-2 space-y-1`}>
-            <div className="flex justify-between">
-              <Toggle
-                checked={routes.activeRoutes.includes(route.id)}
-                onChange={(checked) =>
-                  checked
-                    ? routes.addActiveRoute(route.id)
-                    : routes.removeActiveRoute(route.id)
-                }
-                small
-              />
-              <div className="space-x-3">
-                {"update" in router ? (
+        {routes.routes.map((route) => {
+          const filename = `route_${route.name
+            .replaceAll(/[^a-z0-9]+/gi, " ")
+            .replaceAll(/\s/g, "_")}_${route.id}.json`.toLowerCase();
+          return (
+            <article key={route.id} className={`p-2 space-y-1`}>
+              <div className="flex justify-between">
+                <Toggle
+                  checked={routes.activeRoutes.includes(route.id)}
+                  onChange={(checked) =>
+                    checked
+                      ? routes.addActiveRoute(route.id)
+                      : routes.removeActiveRoute(route.id)
+                  }
+                  small
+                />
+                <div className="space-x-3">
+                  {"update" in router ? (
+                    <button
+                      className="hover:text-white"
+                      onClick={() => {
+                        overwolf.io.writeFileContents(
+                          `${overwolf.io.paths.documents}\\Diablo 4 Map\\${filename}`,
+                          JSON.stringify(route),
+                          "UTF8" as overwolf.io.enums.eEncoding.UTF8,
+                          true,
+                          () => console.log
+                        );
+                        overwolf.utils.openWindowsExplorer(
+                          `${overwolf.io.paths.documents}\\Diablo 4 Map`,
+                          console.log
+                        );
+                      }}
+                    >
+                      Export
+                    </button>
+                  ) : (
+                    <a
+                      href={`data:text/json;charset=utf-8,${encodeURIComponent(
+                        JSON.stringify(route)
+                      )}`}
+                      download={filename}
+                      className="hover:text-white"
+                    >
+                      Export
+                    </a>
+                  )}
                   <button
                     className="hover:text-white"
                     onClick={() => {
-                      overwolf.io.writeFileContents(
-                        `${overwolf.io.paths.documents}\\Diablo 4 Map\\route_${route.id}.json`,
-                        JSON.stringify(route),
-                        "UTF8" as overwolf.io.enums.eEncoding.UTF8,
-                        true,
-                        () => console.log
-                      );
-                      overwolf.utils.openWindowsExplorer(
-                        `${overwolf.io.paths.documents}\\Diablo 4 Map`,
-                        console.log
-                      );
+                      routes.updateTempRoute(route);
+                      routes.setIsCreating(true);
                     }}
                   >
-                    Export
+                    Edit
                   </button>
-                ) : (
-                  <a
-                    href={`data:text/json;charset=utf-8,${encodeURIComponent(
-                      JSON.stringify(route)
-                    )}`}
-                    download={`route_${route.id}.json`}
+                  <button
                     className="hover:text-white"
+                    onClick={() => {
+                      if (
+                        confirm("Are you sure you want to delete this route?")
+                      ) {
+                        routes.removeRoute(route.id);
+                      }
+                    }}
                   >
-                    Export
-                  </a>
-                )}
-                <button
-                  className="hover:text-white"
-                  onClick={() => {
-                    routes.updateTempRoute(route);
-                    routes.setIsCreating(true);
-                  }}
-                >
-                  Edit
-                </button>
-                <button
-                  className="hover:text-white"
-                  onClick={() => {
-                    if (
-                      confirm("Are you sure you want to delete this route?")
-                    ) {
-                      routes.removeRoute(route.id);
-                    }
-                  }}
-                >
-                  Delete
-                </button>
+                    Delete
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="truncate text-base">{route.name}</div>
-            <RouteTypes route={route} />
-          </article>
-        ))}
+              <div className="truncate text-base">{route.name}</div>
+              <RouteTypes route={route} />
+            </article>
+          );
+        })}
       </div>
     </div>
   );
