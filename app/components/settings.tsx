066b50227bd8c49e1b7390d32bc972aa2bf6352f
lmachens <leon@machens.koeln>
2023-07-31 21:08:37 +0200

    Revert formatting

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -1,61 +1,57 @@
-import { useGlobalSettingsStore } from '../lib/storage/global-settings';
-import Toggle from './toggle';
-import { useDict } from '../components/(i18n)/i18n-provider';
+import { useDict } from "../components/(i18n)/i18n-provider";
+import { useGlobalSettingsStore } from "../lib/storage/global-settings";
+import Toggle from "./toggle";
 
 export default function Settings() {
-	const globalSettingsStore = useGlobalSettingsStore();
-	const dict = useDict();
+  const globalSettingsStore = useGlobalSettingsStore();
+  const dict = useDict();
 
-	return (
-		<>
-			<div className="flex">
-				<span className="w-1/2">{dict.settings.showEventsTimers}</span>
-				<Toggle
-					checked={!!globalSettingsStore.showTimers}
-					onChange={globalSettingsStore.toggleShowTimers}
-				/>
-			</div>
-			<div className="flex">
-				<span className="w-1/2">{dict.settings.stackEventsTimers}</span>
-				<Toggle
-					checked={!!globalSettingsStore.stackTimers}
-					onChange={globalSettingsStore.toggleStackTimers}
-				/>
-			</div>
-			<div className="flex">
-				<span className="w-1/2">
-					{dict.settings.showTerritoryNames}
-				</span>
-				<Toggle
-					checked={!!globalSettingsStore.showTerritoryNames}
-					onChange={globalSettingsStore.toggleShowTerritoryNames}
-				/>
-			</div>
-			<div className="flex">
-				<span className="w-1/2">
-					{dict.settings.alternativeDiscoveredWaypointsIcon}
-				</span>
-				<Toggle
-					checked={!!globalSettingsStore.isAlternativeDiscoveredStyle}
-					onChange={
-						globalSettingsStore.toggleIsAlternativeDiscoveredStyle
-					}
-				/>
-			</div>
-			<div className="flex">
-				<span className="w-1/2">{dict.settings.iconSize}</span>
-				<input
-					className="w-5/12"
-					type="range"
-					value={globalSettingsStore.iconSize}
-					min={0.4}
-					max={2}
-					step={0.1}
-					onChange={(event) =>
-						globalSettingsStore.setIconSize(+event.target.value)
-					}
-				/>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="flex">
+        <span className="w-1/2">{dict.settings.showEventsTimers}</span>
+        <Toggle
+          checked={!!globalSettingsStore.showTimers}
+          onChange={globalSettingsStore.toggleShowTimers}
+        />
+      </div>
+      <div className="flex">
+        <span className="w-1/2">{dict.settings.stackEventsTimers}</span>
+        <Toggle
+          checked={!!globalSettingsStore.stackTimers}
+          onChange={globalSettingsStore.toggleStackTimers}
+        />
+      </div>
+      <div className="flex">
+        <span className="w-1/2">{dict.settings.showTerritoryNames}</span>
+        <Toggle
+          checked={!!globalSettingsStore.showTerritoryNames}
+          onChange={globalSettingsStore.toggleShowTerritoryNames}
+        />
+      </div>
+      <div className="flex">
+        <span className="w-1/2">
+          {dict.settings.alternativeDiscoveredWaypointsIcon}
+        </span>
+        <Toggle
+          checked={!!globalSettingsStore.isAlternativeDiscoveredStyle}
+          onChange={globalSettingsStore.toggleIsAlternativeDiscoveredStyle}
+        />
+      </div>
+      <div className="flex">
+        <span className="w-1/2">{dict.settings.iconSize}</span>
+        <input
+          className="w-5/12"
+          type="range"
+          value={globalSettingsStore.iconSize}
+          min={0.4}
+          max={2}
+          step={0.1}
+          onChange={(event) =>
+            globalSettingsStore.setIconSize(+event.target.value)
+          }
+        />
+      </div>
+    </>
+  );
 }
--- app/lib/i18n/index.ts
+++ app/lib/i18n/index.ts
@@ -1,37 +1,37 @@
-import de from './dictionaries/de.json';
-import en from './dictionaries/en.json';
-import es from './dictionaries/es.json';
-import fr from './dictionaries/fr.json';
-import ru from './dictionaries/ru.json';
-import br from './dictionaries/br.json';
+import br from "./dictionaries/br.json";
+import de from "./dictionaries/de.json";
+import en from "./dictionaries/en.json";
+import es from "./dictionaries/es.json";
+import fr from "./dictionaries/fr.json";
+import ru from "./dictionaries/ru.json";
 
 export type DICT =
-	| typeof en
-	| typeof de
-	| typeof es
-	| typeof fr
-	| typeof ru
-	| typeof br;
+  | typeof en
+  | typeof de
+  | typeof es
+  | typeof fr
+  | typeof ru
+  | typeof br;
 const DICTIONARIES = {
-	en,
-	de,
-	es,
-	fr,
-	ru,
-	br,
+  en,
+  de,
+  es,
+  fr,
+  ru,
+  br,
 } as const;
 
 export const LOCALES = Object.keys(DICTIONARIES);
-export const DEFAULT_LOCALE = 'en';
+export const DEFAULT_LOCALE = "en";
 
 export const isLang = (lang?: string) => {
-	return typeof lang !== 'undefined' && LOCALES.includes(lang);
+  return typeof lang !== "undefined" && LOCALES.includes(lang);
 };
 
 export const loadDictionary = (lang = DEFAULT_LOCALE) => {
-	if (!isLang(lang)) {
-		return DICTIONARIES[DEFAULT_LOCALE];
-	}
+  if (!isLang(lang)) {
+    return DICTIONARIES[DEFAULT_LOCALE];
+  }
 
-	return DICTIONARIES[lang as keyof typeof DICTIONARIES];
+  return DICTIONARIES[lang as keyof typeof DICTIONARIES];
 };
